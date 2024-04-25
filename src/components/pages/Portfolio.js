7361c544f1d17aa61dd1db586ccbe2d38a958ba0
NikPratr <NikPrater@gmail.com>
2024-04-25 19:41:04 -0400

    Added music website

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,7 +3,7 @@ import characterCards from '../../images/character-cards.png';
 import socialMedia from '../../images/social-media-project1.png';
 import textEditor from '../../images/text-editor-pwa.png';
 import ormDemo from '../../images/orm-demonstration.png';
-import bbPlaza from '../../images/bb-plaza.png';
+import musicWebsite from '../../images/music-website-image.png'
 
 import Project from '../Project';
 
@@ -12,6 +12,13 @@ export default function Portfolio() {
     <div>
       <div className='content-body'>
         <div id='portfolio-projects'>
+          <Project 
+          title='Mock Music Website'
+          picture={musicWebsite}
+          description="I'm working on a website as a creative and technical exploration. It uses HTML, CSS, Typescript, MySQL, and other tech. It is still a work in progress."
+          github={'https://nikpratr.github.io/mock-music-site/'}
+          id={1}
+          />
           <Project 
           title='Character Cards'
           picture={characterCards}
@@ -40,14 +47,6 @@ export default function Portfolio() {
           description='This is an ORM structure that you may find in an e-commerce site. It uses Sequel DB, class models to create and seed tables, and CRUD operations to manipulate data. The tech used in this project includes dotenv, express, mysql2, and sequelize.'
           github='https://github.com/NikPratr/orm-back-end-demonstration'
           id={4}
-          />
-          <Project
-          title='BB Plaza'
-          picture={bbPlaza}
-          description='This is an art social media demo that was created during my time in coding bootcamp. It remains unfinished, but I designed the homepage and logo, and it features the ability to log in/log out. It was built using React, React Router, GraphQL, and more front end and backend tech.'
-          github='https://github.com/DVidal1209/art-museum'
-          deployment={'https://protected-shore-90563.herokuapp.com/'}
-          id={5}
           />
         </div>
       </div>
