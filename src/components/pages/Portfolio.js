d35a2fcd2f329aed45c34e490ce6cf0ad5ce0d2a
NikPratr <NikPrater@gmail.com>
2024-04-29 11:51:17 -0400

    Changed link to music site

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -16,7 +16,7 @@ export default function Portfolio() {
           title='Mock Music Website'
           picture={musicWebsite}
           description="I'm working on a website as a creative and technical exploration. It uses HTML, CSS, Typescript, MySQL, and other tech. It is still a work in progress."
-          github={'https://nikpratr.github.io/mock-music-site/'}
+          github={'https://github.com/NikPratr/mock-music-site'}
           id={1}
           />
           <Project 
@@ -24,29 +24,28 @@ export default function Portfolio() {
           picture={characterCards}
           description='This is a personal project I worked on toward the end of the bootcamp. It is made using HTML, CSS, and Javascript. It also demonstrates data visualization with chart.js.'
           github={'https://github.com/NikPratr/vessel-cards'}
-          id={1}
+          id={2}
           />
           <Project
           title='Social Media Demo'
           picture={socialMedia}
           description='This Bootcamp project is a good demonstartion of ORM, RESTful API, and database design. It uses MongoDB, Mongoose, and Express for full CRUD capability.'
           github='https://github.com/NikPratr/mongo-social-network-api'
-          id={2}
+          id={3}
           />
           <Project
           title='Text Editor PWA'
           picture={textEditor}
           description='This project is a PWA uses service workers, a web app manifest, and data caching strategies.'
           github='https://github.com/NikPratr/text-editor-pwa'
-          deployment={'https://blooming-plains-09501.herokuapp.com'}
-          id={3}
+          id={4}
           />
           <Project
           title='ORM Demo'
           picture={ormDemo}
           description='This is an ORM structure that you may find in an e-commerce site. It uses Sequel DB, class models to create and seed tables, and CRUD operations to manipulate data. The tech used in this project includes dotenv, express, mysql2, and sequelize.'
           github='https://github.com/NikPratr/orm-back-end-demonstration'
-          id={4}
+          id={5}
           />
         </div>
       </div>
